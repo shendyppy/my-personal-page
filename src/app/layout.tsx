15586b314c9ef2fd7c0ae09bd8e7b93d8538591b
Shendy <shendyppy@gmail.com>
2025-08-17 10:32:44 +0700

    feat(initiatialization): initiate and install next.js as core project tech stacks

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Shendy's Personal Page",
+  description:
+    "A personal space to sharpen my skills while exploring and experimenting with new tech stacks I haven’t tried before",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
